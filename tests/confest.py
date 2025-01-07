2a817b65acaf1cd1939ca0d7b60f097687ec3a59
n1figo <n1figo@naver.com>
2025-01-07 02:28:34 +0000

    2501071128

--- tests/confest.py
+++ tests/confest.py
@@ -8,4 +8,4 @@ def test_data_dir():
 
 @pytest.fixture
 def sample_pdf_path(test_data_dir):
-    return test_data_dir / "basic_table.pdf"
+    return test_data_dir / "KB 금쪽같은 자녀보험Plus(무배당)(24.05)_11월11일판매_요약서_v1.1.pdf"
